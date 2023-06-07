f5152b357ee0094142aafed18130fa1811857e6b
Thanhdc <thanhdc@bap.jp>
2023-06-07 17:25:36 +0700

    update

--- reactfansite/src/decryptPath.js
+++ reactfansite/src/decryptPath.js
@@ -2,7 +2,7 @@ import CryptoJS from "crypto-js";
 
 const decryptPath = function (pathEncrypted) {
   // The APP_KEY in .env file. Note that it is base64 encoded binary
-  var key = 'EMPOjqdtKqM/dNO3nWw6GNNjBCYQjmtolhNP5qRaYB0=';
+  var key = 'BvCBhMqzXCiGUaecKIxE551Dx4ueA64VJumfMsVyk9k=';
 
   // Laravel creates a JSON to store iv, value and a mac and base64 encodes it.
   // So let's base64 decode the string to get them.
